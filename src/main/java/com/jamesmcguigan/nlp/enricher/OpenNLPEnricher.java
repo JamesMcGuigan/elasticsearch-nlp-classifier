5cc64eb5b1444d927ed84af1cb22f843422edb78
JamesMcGuigan <james.mcguigan.github@gmail.com>
2021-02-10 21:37:59 +0000

    OpenNLPEnricher | implement asyncUpdate() with rate limiting | slow but works

--- src/main/java/com/jamesmcguigan/nlp/enricher/OpenNLPEnricher.java
+++ src/main/java/com/jamesmcguigan/nlp/enricher/OpenNLPEnricher.java
@@ -2,20 +2,31 @@ package com.jamesmcguigan.nlp.enricher;
 
 import com.jamesmcguigan.nlp.classifier.OpenNLPClassifierES;
 import com.jamesmcguigan.nlp.csv.ESJsonPath;
+import com.jamesmcguigan.nlp.elasticsearch.ESClient;
 import com.jamesmcguigan.nlp.elasticsearch.ScanAndScrollRequest;
 import com.jamesmcguigan.nlp.streams.ESDocumentStream;
+import org.elasticsearch.ElasticsearchStatusException;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
-import java.util.TreeMap;
+import java.util.*;
 
-import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
 import static org.elasticsearch.index.query.QueryBuilders.existsQuery;
 
+/**
+ *  OpenNLPEnricher streams from ElasticSearch via `ESDocumentStream()`
+ *  trains on data extracted via `target` field,
+ *  predicts using `OpenNLPClassifierES()`
+ *  updates ElasticSearch with new prediction metadata
+ */
 @SuppressWarnings("unchecked")
 public class OpenNLPEnricher {
 
@@ -24,12 +35,16 @@ public class OpenNLPEnricher {
     private final String       target;
     private final String       prefix;
 
+    private final int minRequestsInFlight = 5;
+    private int maxRequestsInFlight = 25;
+    private int requestsInFlight    = 0;
+
     private double accuracy = 0.0;
     private final OpenNLPClassifierES classifier = new OpenNLPClassifierES();
 
 
     public OpenNLPEnricher(String index, List<String> fields, String target) { this(index, fields, target, null); }
-    public OpenNLPEnricher(String index, List<String> fields, String target, String prefix) {
+    public OpenNLPEnricher(String index, List<String> fields, String target, @Nullable String prefix) {
         this.index  = index;
         this.fields = fields;
         this.target = target;
@@ -40,11 +55,15 @@ public class OpenNLPEnricher {
     public double getAccuracy() { return this.accuracy; }
 
 
-    public <T extends OpenNLPEnricher> T train() throws IOException {
+    public <T extends OpenNLPEnricher> T train() throws IOException { return train(null); }
+    public <T extends OpenNLPEnricher> T train(@Nullable QueryBuilder query) throws IOException {
+        BoolQueryBuilder streamQuery = new BoolQueryBuilder();
+        streamQuery.must(existsQuery(target));
+        if( query != null ) { streamQuery.must(query); }
+
         try (
             ESDocumentStream stream = new ESDocumentStream(
-                index, fields, target,
-                boolQuery().must(existsQuery(target))
+                index, fields, target, streamQuery
             ).setTokenizer(classifier.tokenizer)
         ) {
             classifier.train(stream);
@@ -54,7 +73,7 @@ public class OpenNLPEnricher {
 
 
     public <T extends OpenNLPEnricher> T enrich() throws IOException { return enrich(null); }
-    public <T extends OpenNLPEnricher> T enrich(QueryBuilder query) throws IOException {
+    public <T extends OpenNLPEnricher> T enrich(@Nullable QueryBuilder query) throws IOException {
         ScanAndScrollRequest<String> request = new ScanAndScrollRequest<>(index, query, String.class);
 
         int correct = 0;
@@ -62,12 +81,14 @@ public class OpenNLPEnricher {
         while( request.hasNext() ) {
             String json       = request.next();
             var jsonPath      = new ESJsonPath(json);
-            String id         = jsonPath.get("_id");
+            String id         = jsonPath.get("id");
             String category   = jsonPath.get(this.target);
             String[] tokens   = jsonPath.tokenize(this.fields).toArray(new String[0]);
             String prediction = this.classifier.predict(tokens);
 
-            this.queueUpdate(id, this.target, prediction);
+            if( this.isUpdateRequired(jsonPath, target, prediction) ) {
+                this.asyncUpdate(id, this.target, prediction);
+            }
 
             if( !category.isEmpty() ) {
                 correct += category.equals(prediction) ? 1 : 0;
@@ -77,10 +98,86 @@ public class OpenNLPEnricher {
         this.accuracy = (count > 0) ? (double) correct / count : 0.0;
         return (T) this;
     }
+    private String getPredictionPath(String target) {
+        return this.prefix.isEmpty() ? target : this.prefix+'.'+target;
+    }
+    private boolean isUpdateRequired(ESJsonPath jsonPath, String target, String prediction) {
+        String path     = this.getPredictionPath(target);
+        String existing = jsonPath.get(path);
+        return !prediction.equals(existing);
+    }
 
 
-    private void queueUpdate(String id, String target, String value) {
-        // TODO: implement
+    @SuppressWarnings("BusyWait")
+    private void waitForQueue() {
+        while( this.requestsInFlight > Math.max(this.maxRequestsInFlight, this.minRequestsInFlight) ) {
+            try {
+                Thread.sleep(10);
+            } catch( InterruptedException e ) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+    private UpdateRequest getUpdateRequest(String id, String target, String value) {
+        Map<String, Object> jsonMap   = new HashMap<>();
+        Map<String, Object> targetMap = new HashMap<>();
+        jsonMap.put(this.prefix, targetMap);
+        targetMap.put(target, value);
+        if( this.prefix.isEmpty() ) {
+            jsonMap = targetMap;
+        }
+        UpdateRequest request = new UpdateRequest(this.index, id).doc(jsonMap);
+        return request;
+    }
+    private void asyncUpdate(String id, String target, String value) throws IOException {
+        // TODO: implement bulk update
+        this.waitForQueue();
+
+        this.requestsInFlight += 1;
+        UpdateRequest request = this.getUpdateRequest(id, target, value);
+        ESClient.getInstance().updateAsync(
+            request,
+            RequestOptions.DEFAULT,
+            new ActionListener<>() {
+                @Override
+                public void onResponse(UpdateResponse updateResponse) {
+                    OpenNLPEnricher.this.requestsInFlight -= 1;
+
+                    var result = updateResponse.getResult();
+                    System.out.printf("%s %s(%s) | %s.%s = %s%n",
+                        result.toString(), index, id, OpenNLPEnricher.this.prefix, target, value
+                    );
+                }
+                @Override
+                public void onFailure(Exception e) {
+                    OpenNLPEnricher.this.requestsInFlight -= 1;
+                    boolean retry  = false;
+                    String status  = "ERROR";
+                    String message = e.toString();
+
+                    // ERROR: Concurrent request limit exceeded. Please consider batching your requests
+                    // WORKAROUND: Retry and reduce maxRequestsInFlight
+                    if( e instanceof ElasticsearchStatusException ) {
+                        var exception = (ElasticsearchStatusException) e;
+                        if( exception.status().toString().equals("TOO_MANY_REQUESTS") ) {
+                            OpenNLPEnricher.this.maxRequestsInFlight *= 0.9;  // race condition is desirable here
+                            retry   = true;
+                            status  = exception.status().toString();
+                            message = "reducing maxRequestsInFlight = " + OpenNLPEnricher.this.maxRequestsInFlight;
+                        }
+                    }
+
+                    System.out.printf("%s %s(%s) | %s%n", status, index, id, message);
+                    if( retry ) {
+                        try {
+                            OpenNLPEnricher.this.asyncUpdate(id, target, value);
+                        } catch( IOException ioException ) {
+                            ioException.printStackTrace();
+                        }
+                    }
+                }
+            }
+        );
     }
 
 
@@ -95,6 +192,7 @@ public class OpenNLPEnricher {
             accuracies.put(target, accuracy);
         }
         String className = MethodHandles.lookup().lookupClass().getSimpleName();
-        System.out.printf("%s() accuracy for on training data: %s", className, accuracies.toString());
+        System.out.printf("%s() accuracy for on training data: %s%n", className, accuracies.toString());
+        ESClient.getInstance().close();
     }
 }
