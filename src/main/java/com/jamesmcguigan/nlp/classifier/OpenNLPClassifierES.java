b861145546031acf1ca42da5a6bf604efef7763e
JamesMcGuigan <james.mcguigan.github@gmail.com>
2021-02-15 16:11:28 +0000

    OpenNLPClassifierES | refactor as kFoldValidation() method

--- src/main/java/com/jamesmcguigan/nlp/classifier/OpenNLPClassifierES.java
+++ src/main/java/com/jamesmcguigan/nlp/classifier/OpenNLPClassifierES.java
@@ -13,39 +13,52 @@ import static org.elasticsearch.index.query.QueryBuilders.*;
 
 public class OpenNLPClassifierES extends OpenNLPClassifier {
 
+    public double kFoldValidation(String index, List<String> fields, String target, int folds) throws IOException {
+        double[] accuracies = new double[folds];
+        long[]   trainHits  = new long[folds];
+        long[]   testHits   = new long[folds];
+
+        for( int fold = 0; fold < folds; fold++ ) {
+            try(
+                ESDocumentStream trainStream = new ESDocumentStream(
+                    index, fields, target,
+                    boolQuery()
+                        .must(existsQuery(target))
+                        .mustNot(scriptQuery(new Script(
+                            "Integer.parseInt(doc['_id'].value) % "+folds+" == "+fold))
+                        )
+                ).setTokenizer(this.tokenizer);
+
+                ESDocumentStream testStream = new ESDocumentStream(
+                    index, fields, target,
+                    boolQuery()
+                        .must(existsQuery(target))
+                        .must(scriptQuery(new Script(
+                            "Integer.parseInt(doc['_id'].value) % "+folds+" == "+fold))
+                        )
+                ).setTokenizer(this.tokenizer)
+            ) {
+                // TODO: multi-target classification
+                this.train(trainStream);
+                accuracies[fold] = this.accuracy(testStream);
+                trainHits[fold]  = trainStream.getTotalHits();
+                testHits[fold]   = testStream.getTotalHits();
+            }
+        }
+
+        double accuracy    = Arrays.stream(accuracies).average().orElse(0);
+        long meanTrainHits = (long) Arrays.stream(trainHits).average().orElse(0);
+        long meanTestHits  = (long) Arrays.stream(testHits).average().orElse(0);
+        String className = MethodHandles.lookup().lookupClass().getSimpleName();
+        System.out.printf("%s() accuracy on %d folds (%d/%d split) is %.3f%n",
+            className, folds, meanTestHits, meanTrainHits, accuracy);
+
+        return accuracy;
+    }
+
     public static void main(String[] args) throws IOException {
         var classifier = new OpenNLPClassifierES();
-        long split     = Math.round(1 / 0.2);
-        String index   = "twitter";
-        String target  = "target";
-        List<String> fields = Arrays.asList("text", "location");
-
-        try(
-            ESDocumentStream trainStream = new ESDocumentStream(
-                index, fields, target,
-                boolQuery()
-                    .must(existsQuery(target))
-                    .mustNot(scriptQuery(new Script("Integer.parseInt(doc['_id'].value) % "+split+" == 0")))
-            ).setTokenizer(classifier.tokenizer);
-
-            ESDocumentStream testStream = new ESDocumentStream(
-                index, fields, target,
-                boolQuery()
-                    .must(existsQuery(target))
-                    .must(scriptQuery(new Script("Integer.parseInt(doc['_id'].value) % "+split+" == 0")))
-            ).setTokenizer(classifier.tokenizer)
-        ) {
-            // TODO: multi-target classification
-            classifier.train(trainStream);
-            double accuracy = classifier.accuracy(testStream);
-
-            String className = MethodHandles.lookup().lookupClass().getSimpleName();
-            System.out.printf(
-                "%s() accuracy on %d/%d test-train-split is %.3f%n",
-                className, testStream.getTotalHits(), trainStream.getTotalHits(), accuracy
-            );
-        } finally {
-            ESClient.getInstance().close();
-        }
+        classifier.kFoldValidation("twitter", Arrays.asList("text", "location"), "target", 3);
+        ESClient.getInstance().close();
     }
 }
