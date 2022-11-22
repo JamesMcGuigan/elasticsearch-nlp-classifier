c1f60939a8bf49c5f444356e33395d7ac9296f2b
James McGuigan <james.mcguigan1@nhs.net>
2022-11-22 19:31:00 +0000

    download.sh | unzip input/ + input/nlp-getting-started/

--- download.sh
+++ download.sh
@@ -4,4 +4,5 @@
 mkdir -p input/
 kaggle c download nlp-getting-started -p input/
 unzip input/nlp-getting-started.zip -d input/
+unzip input/nlp-getting-started.zip -d input/nlp-getting-started/  # v1 + v2 assume different paths
 rm -f input/nlp-getting-started.zip
